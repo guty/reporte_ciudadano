78ca1e3370e362ce7b0f9da8db081d4142804f04
Guty Santana <agustin.santana@gmail.com>
2022-01-22 14:10:04 -0600

    Added Role to Users table

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def resource
     @resource ||= build(:user)
   end
 
-  test "must be a valid resource" do
+  test 'must be a valid resource' do
     assert resource.valid?
+  end
+
+  test 'must be guest as default role' do
+    assert resource.guest?
   end
 end
