8f1b652e5f4b04d19486659b832738ef6e83e76a
Agustin Santana <agustin.santana@gmail.com>
2022-01-23 17:52:02 -0600

    Seed for Users table, change of Roles values and policy (#8)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+users = User.find_or_create_by(email: 'admin@mail.com') do |u|
+  u.password = 'admin1234'
+  u.role = :admin
+end
