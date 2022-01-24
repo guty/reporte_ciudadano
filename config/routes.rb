281a7682836e3da09826459bf23f235055efc1ff
Guty Santana <agustin.santana@gmail.com>
2022-01-23 21:16:41 -0600

    Categories complaints scaffold generated with tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'landing#index'
   devise_for :users
-  root "landing#index"
+  resources :categories
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,18 @@ users = User.find_or_create_by(email: 'admin@mail.com') do |u|
   u.password = 'admin1234'
   u.role = :admin
 end
+
+Category.destroy_all
+
+Category.create!([{
+                   name: 'Baches',
+                   active: true
+                 },
+                  {
+                    name: 'Alumbrado público',
+                    active: true
+                  },
+                  {
+                    name: 'Parques y jardínes',
+                    active: true
+                  }])
