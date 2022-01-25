b924e311148499ceb816ea570ac53d91ffbd3313
Guty Santana <agustin.santana@gmail.com>
2022-01-25 13:49:42 -0600

    Categories CRUD generated through scaffolding; DISCLAIMER: previous commit is about Dependencies, not Categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_180013) do
+ActiveRecord::Schema.define(version: 2022_01_25_190400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2022_01_25_180013) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.boolean "active", default: false
+    t.boolean "active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ test/factories/categories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :category do
+    name { "Baches" }
+    active { true }
+  end
+end
