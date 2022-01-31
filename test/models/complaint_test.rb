b86cfeb5553c7ed423acf769a19b7667916d349f
Guty Santana <agustin.santana@gmail.com>
2022-01-30 21:46:33 -0600

    Complaint model files generated (with scaffold)

--- test/models/complaint_test.rb
+++ test/models/complaint_test.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class ComplaintTest < ActiveSupport::TestCase
   def resource
     @resource ||= build(:complaint)
   end
 
-  test "must be a valid resource" do
+  test 'must be a valid resource' do
     assert resource.valid?
   end
 end
