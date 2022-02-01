908dbf8e73beddfac4bbd29f413f62ab362163e7
Guty Santana <agustin.santana@gmail.com>
2022-02-01 11:40:41 -0600

    Rubocop applied

--- app/helpers/complaints_helper.rb
+++ app/helpers/complaints_helper.rb
@@ -2,6 +2,6 @@
 
 module ComplaintsHelper
   def category_options_for(categories)
-    options_from_collection_for_select(categories, 'id', 'name')
+    options_from_collection_for_select(categories, "id", "name")
   end
 end
