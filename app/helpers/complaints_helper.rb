c3f60f981aa9372e624d5ec3d70ade718520129f
Guty Santana <agustin.santana@gmail.com>
2022-02-01 11:38:00 -0600

    Attending Pull Request comments, created authorization controller, optimized code in complaint controller and new complaint view, added complaint helper, removed migrations files, changes applied direct to main files and new application logo added to NavBar

--- app/helpers/complaints_helper.rb
+++ app/helpers/complaints_helper.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module ComplaintsHelper
-  STATUSES_CSS = { created: "is-dark",
-                   in_process: "is-warning",
-                   attended: "is-info",
-                   attended_by_program: "is-info",
-                   rejected: "is-danger" }.freeze
+  STATUSES_CSS = { created: 'is-dark',
+                   in_process: 'is-warning',
+                   attended: 'is-info',
+                   attended_by_program: 'is-info',
+                   rejected: 'is-danger' }.freeze
 
   def category_options_for(categories)
-    options_from_collection_for_select(categories, "id", "name")
+    options_from_collection_for_select(categories, 'id', 'name')
   end
 
   def status_name(status)
--- db/migrate/20220201004638_add_dependency_to_category.rb
+++
@@ -1,5 +0,0 @@
-class AddDependencyToCategory < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :categories, :dependency
-  end
-end
