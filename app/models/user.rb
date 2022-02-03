6455bf0211525b3aa49738b55252187efccba6b6
Guty Santana <agustin.santana@gmail.com>
2022-02-03 08:36:58 -0600

    Implemented File Attachment (user's avatar) feature and citizen's profile update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :complaints, dependent: :nullify
+  has_one_attached :avatar
 
   enum :role, { citizen: 0, employee: 1, director: 2, admin: 3 }
 end
