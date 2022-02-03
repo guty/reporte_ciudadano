b1d57f72b7118e8ca878bf94a9ccd46af572ace5
Guty Santana <agustin.santana@gmail.com>
2022-02-03 08:36:58 -0600

    Implemented File Attachment (user's avatar) feature and citizen's profile update

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Cloudinary.config do |config|
+  config.cloud_name = Rails.application.credentials.dig(:cloudinary, :cloud_name)
+  config.api_key = Rails.application.credentials.dig(:cloudinary, :api_key)
+  config.secret_access_key = Rails.application.credentials.dig(:cloudinary, :api_secret)
+  config.secure = true
+  config.cdn_subdomain = true
+end
