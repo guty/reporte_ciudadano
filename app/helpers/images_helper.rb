7e6c18358256d92269f35e5e770d4429858101be
Guty Santana <agustin.santana@gmail.com>
2022-02-04 15:30:24 -0600

    Image Helper refactor

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -2,17 +2,20 @@
 
 module ImagesHelper
   def display_image(image, image_params)
-    if Rails.env.development?
-      image_tag(image.variant(resize_to_fill: [image_params[:width], image_params[:height]]),
-                class: image_params[:class])
-    elsif Rails.env.production?
-      if image_params[:avatar]
-        image_tag(image.key, type: :upload, width: image_params[:width], height: image_params[:height],
-                             gravity: 'faces', crop: :thumb, class: image_params[:class])
-      else
-        image_tag(image.key, type: :upload, width: image_params[:width], height: image_params[:height],
-                             crop: :fill, class: image_params[:class])
-      end
-    end
+    return if Rails.env.development?
+
+    image_tag(image.variant(resize_to_fill: [image_params[:width], image_params[:height]]),
+              class: image_params[:class])
+
+    return if Rails.env.production?
+    return display_avatar(image, image_params) if image_params[:avatar]
+
+    image_tag(image.key, type: :upload, width: image_params[:width], height: image_params[:height], crop: :fill,
+                         class: image_params[:class])
+  end
+
+  def display_avatar(image, image_params)
+    image_tag(image.key, type: :upload, width: image_params[:width], height: image_params[:height], gravity: "faces",
+                         crop: :thumb, class: image_params[:class])
   end
 end
