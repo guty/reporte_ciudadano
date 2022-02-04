f3647d4ff525e7a022f7f138e9ed24fe29662838
Guty Santana <agustin.santana@gmail.com>
2022-02-04 16:40:01 -0600

    Fix to Image helper

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -2,20 +2,8 @@
 
 module ImagesHelper
   def display_image(image, image_params)
-    return if Rails.env.development?
+    return image_tag(image.key, image_params) if Rails.env.production?
 
-    image_tag(image.variant(resize_to_fill: [image_params[:width], image_params[:height]]),
-              class: image_params[:class])
-
-    return if Rails.env.production?
-    return display_avatar(image, image_params) if image_params[:avatar]
-
-    image_tag(image.key, type: :upload, width: image_params[:width], height: image_params[:height], crop: :fill,
-                         class: image_params[:class])
-  end
-
-  def display_avatar(image, image_params)
-    image_tag(image.key, type: :upload, width: image_params[:width], height: image_params[:height], gravity: "faces",
-                         crop: :thumb, class: image_params[:class])
+    image_tag(image.variant(resize_to_fill: [image_params[:width], image_params[:height]]), class: image_params[:class])
   end
 end
