1df17be53bb9adbca76ca329d88e78046d28c9be
Guty Santana <agustin.santana@gmail.com>
2022-02-04 15:01:29 -0600

    Adding key to Avatar object to avoid send the object instead the key to Cloudinary

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -7,11 +7,11 @@ module ImagesHelper
                 class: image_params[:class])
     elsif Rails.env.production?
       if image_params[:avatar]
-        image_tag(image, type: :upload, width: image_params[:width], height: image_params[:height], gravity: 'faces',
-                         crop: :thumb, class: image_params[:class])
+        image_tag(image.key, type: :upload, width: image_params[:width], height: image_params[:height],
+                             gravity: 'faces', crop: :thumb, class: image_params[:class])
       else
-        image_tag(image.key, type: :upload, width: image_params[:width], height: image_params[:height], crop: :fill,
-                             class: image_params[:class])
+        image_tag(image.key, type: :upload, width: image_params[:width], height: image_params[:height],
+                             crop: :fill, class: image_params[:class])
       end
     end
   end
